d49f6c9f3e9a028d04fde04e3da1de1a1daeb49f
Dushyant Yadav <dushyant9309@gmail.com>
2020-04-18 17:28:19 +0530

    Use print statements instead of loggig

--- src/controller/switch_stats.py
+++ src/controller/switch_stats.py
@@ -28,12 +28,12 @@ class SwitchStats:
     
     def add_stat(self, body):
 
-        self.logger.info('datapath         '
-                         'in-port  eth-dst           '
-                         'out-port packets  bytes')
-        self.logger.info('---------------- '
-                         '-------- ----------------- '
-                         '-------- -------- --------')
+        print('datapath         '
+              'in-port  eth-dst           '
+              'out-port packets  bytes')
+        print('---------------- '
+              '-------- ----------------- '
+              '-------- -------- --------')
 
         switch_byte_count = 0
         switch_packet_count = 0
@@ -41,11 +41,11 @@ class SwitchStats:
         for stat in sorted([flow for flow in body if flow.priority >= 1],
                            key=lambda flow: (flow.match['in_port'],
                                              flow.match['eth_dst'])):
-            self.logger.info('%016x %8x %17s %8x %8d %8d',
-                             self.datapath.id,
-                             stat.match['in_port'], stat.match['eth_dst'],
-                             stat.instructions[0].actions[0].port,
-                             stat.packet_count, stat.byte_count)
+            print('%016x %8x %17s %8x %8d %8d',
+                   self.datapath.id,
+                   stat.match['in_port'], stat.match['eth_dst'],
+                   stat.instructions[0].actions[0].port,
+                   stat.packet_count, stat.byte_count)
             switch_byte_count += stat.byte_count
             switch_packet_count += stat.packet_count
 
